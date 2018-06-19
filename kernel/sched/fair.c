1aad360794086f6cd3ee4554909925ade0a123e1
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2018-06-19 14:19:29 -0700

    sched: improve trace prints
    Add the target and backup cpu in find_best_target trace print.
    During placement print more information about the cpu before
    skipping it. This helps in understanding why the cpu was not
    considered for placement.
    
    Change-Id: Ifc4de6b1d7495d2d701a92f465f4c81e86f468ea
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7002,6 +7002,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util;
 
+			trace_sched_cpu_util(i);
+
 			if (!cpu_online(i) || cpu_isolated(i))
 				continue;
 
@@ -7018,8 +7020,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 			if (sched_cpu_high_irqload(i))
 				continue;
-
-			trace_sched_cpu_util(i);
 
 			/*
 			 * p's blocked utilization is still accounted for on prev_cpu
@@ -7079,7 +7079,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					trace_sched_find_best_target(p,
 							prefer_idle, min_util,
 							cpu, best_idle_cpu,
-							best_active_cpu, i);
+							best_active_cpu,
+							-1, i, -1);
 
 					return i;
 				}
@@ -7247,7 +7248,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 	trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
 				     best_idle_cpu, best_active_cpu,
-				     target_cpu);
+				     most_spare_cap_cpu,
+				     target_cpu,
+				     *backup_cpu);
 
 	schedstat_inc(p->se.statistics.nr_wakeups_fbt_count);
 	schedstat_inc(this_rq()->eas_stats.fbt_count);
