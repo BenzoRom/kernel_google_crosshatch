a24bf15c3dfc6271d2d65945cf31676edeee082a
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-08-20 12:59:59 +0530

    sched: walt: Optimize task_util()
    task_util() for WALT is currently defined as
    
    p->ravg.demand / (sched_ravg_window >> SCHED_CAPACITY_SHIFT);
    
    This math is required to scale the task demand to 1024 scale.
    task_util() is used many times in task placement. So the calls
    to this can be optimized by caching the scaled value when task
    demand is calculated.
    
    Change-Id: I0c170a10704ae3e8fe4e9f271e8e65c3923075e5
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -236,6 +236,9 @@ static int __init set_sched_predl(char *str)
 }
 early_param("sched_predl", set_sched_predl);
 
+__read_mostly unsigned int walt_scale_demand_divisor;
+#define scale_demand(d) ((d)/walt_scale_demand_divisor)
+
 void inc_rq_walt_stats(struct rq *rq, struct task_struct *p)
 {
 	inc_nr_big_task(&rq->walt_stats, p);
@@ -1762,6 +1765,7 @@ static void update_history(struct rq *rq, struct task_struct *p,
 	}
 
 	p->ravg.demand = demand;
+	p->ravg.demand_scaled = scale_demand(demand);
 	p->ravg.coloc_demand = div64_u64(sum, sched_ravg_hist_size);
 	p->ravg.pred_demand = pred_demand;
 
@@ -2016,6 +2020,7 @@ void init_new_task_load(struct task_struct *p, bool idle_task)
 {
 	int i;
 	u32 init_load_windows;
+	u32 init_load_windows_scaled;
 	u32 init_load_pct;
 
 	p->init_load_pct = 0;
@@ -2040,8 +2045,10 @@ void init_new_task_load(struct task_struct *p, bool idle_task)
 
 	init_load_windows = div64_u64((u64)init_load_pct *
 				(u64)sched_ravg_window, 100);
+	init_load_windows_scaled = scale_demand(init_load_windows);
 
 	p->ravg.demand = init_load_windows;
+	p->ravg.demand_scaled = init_load_windows_scaled;
 	p->ravg.coloc_demand = init_load_windows;
 	p->ravg.pred_demand = 0;
 	for (i = 0; i < RAVG_HIST_SIZE_MAX; ++i)
@@ -3297,6 +3304,7 @@ static void walt_init_once(void)
 
 	walt_cpu_util_freq_divisor =
 	    (sched_ravg_window >> SCHED_CAPACITY_SHIFT) * 100;
+	walt_scale_demand_divisor = sched_ravg_window >> SCHED_CAPACITY_SHIFT;
 }
 
 void walt_sched_init_rq(struct rq *rq)
